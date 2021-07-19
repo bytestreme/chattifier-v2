583a325ee300a668df90e3ab48c5350e963c8597
bytestreme <44555427+bytestreme@users.noreply.github.com>
2021-07-20 01:25:02 +0600

    pulsar config to config map

--- socket-api/src/main/java/io/bytestreme/socketapi/config/PulsarConfig.java
+++ socket-api/src/main/java/io/bytestreme/socketapi/config/PulsarConfig.java
@@ -3,6 +3,7 @@ package io.bytestreme.socketapi.config;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.api.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -13,17 +14,26 @@ import java.util.UUID;
 @Configuration
 public class PulsarConfig {
 
-    private static final String SERVICE_URL = "pulsar+ssl://pulsar-gcp-useast4.streaming.datastax.com:6651";
+    private static final String SOCKET_CONSUMER_PREFIX = "socket_consumer_";
+
+    @Value("${pulsar.service-url}")
+    private String serviceUrl;
+
+    @Value("${pulsar.authentication}")
+    private String authentication;
+
+    @Value("${pulsar.topic}")
+    private String topic;
 
     @SneakyThrows
     @Scope("prototype")
     @Bean(destroyMethod = "close")
     public PulsarClient pulsarClient() {
-        log.info("instantiated client");
         return PulsarClient.builder()
-                .serviceUrl(SERVICE_URL)
+                .serviceUrl(serviceUrl)
                 .authentication(
-                        AuthenticationFactory.token("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjbGllbnQ7OTQyZWJiNTktNDQxNC00MGNjLWE0NTktYmJkMjgyN2NhNTVkO2RHNXVkQT09In0.twaKQ-Q6RSH-nIjrgC9Uqpsb02u_v0uYSqoxctASEvJm2MC__9B3u7GSn9X_URQQeBIPtQyKMyR_fOr8WLQXlWI9-QH-sfRSsvtQ3BqLOSh9czOJEWhUXekmcFXG8NK3E6RRB4pQtpSgOi9qMt2kL-Kox2fdbtLmsTuAB0DvjoIp_liF10ST9Pja_aRRO9WS_tCJYwv-rQI4IDB40gZPeC6_qW0uVogh_dqnZtIQ7HnR_2kSNfWkvncDaes6IdN-ehL2phm8bwz8VBxmbmAadN-nZmt9553nRXERcjFuKhSdEPqJCJ9-p4qKq81dvEtbedOaLvl22irvOd-7USsYWA")
+                        AuthenticationFactory
+                                .token(authentication)
                 )
                 .build();
     }
@@ -32,7 +42,7 @@ public class PulsarConfig {
     @Bean(destroyMethod = "close")
     public Producer<byte[]> producer(PulsarClient client) {
         return client.newProducer()
-                .topic("persistent://tnnt/default/my-topic")
+                .topic(topic)
                 .create();
     }
 
@@ -41,16 +51,17 @@ public class PulsarConfig {
     public Consumer<byte[]> consumer(PulsarClient client) {
         MessageListener<byte[]> messageListener = (consumer, msg) -> {
             try {
-                log.info(this.hashCode() + " consumer msg: " + new String(msg.getData()));
+                log.info("consumer msg: " + new String(msg.getData()));
                 consumer.acknowledge(msg);
             } catch (Exception e) {
                 consumer.negativeAcknowledge(msg);
+                log.error(e.getMessage());
             }
         };
 
         return client.newConsumer()
-                .topic("tnnt/default/my-topic")
-                .subscriptionName("socket_consumer_" + UUID.randomUUID())
+                .topic(topic)
+                .subscriptionName(SOCKET_CONSUMER_PREFIX + UUID.randomUUID())
                 .subscriptionType(SubscriptionType.Shared)
                 .messageListener(messageListener)
                 .subscribe();
