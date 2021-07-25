12f317c56b9d2d1f702fb524a99dbb590318c0f9
bytestreme <44555427+bytestreme@users.noreply.github.com>
2021-07-25 19:09:04 +0600

    added metrics service

---
+++ metrics-service/src/main/java/io/bytestreme/metricsservice/MetricsService.java
@@ -0,0 +1,13 @@
+package io.bytestreme.metricsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsService.class, args);
+    }
+
+}
